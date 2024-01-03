083212d83f3205be0cf9d5ef367b5f01347c90e9
lippe-source <81193430+lippe-source@users.noreply.github.com>
2024-01-03 17:57:30 -0300

    Delete exe09.py

--- exe09.py
+++
@@ -1,7 +0,0 @@
-'''
- Faça um Programa que parte a temperatura em graus Farenheit, transforma e
-mostra a temperatura em graus Celsius. C=(5*(F-32)/9).
-'''
-faren = int(input("Digite a temperatura em fahrenheit\n"))
-c=(5*(faren-32)/9)
-print(f"A temperatura em Celcius é de: {c:.1f}")
