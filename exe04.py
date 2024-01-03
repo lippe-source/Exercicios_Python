5fc11f1e1e07577a45ca53794f51c134e927f92a
lippe-source <81193430+lippe-source@users.noreply.github.com>
2024-01-03 17:56:06 -0300

    Delete exe04.py

--- exe04.py
+++
@@ -1,9 +0,0 @@
-'''
-Faça um Programa que peça as 4 notas bimestrais e mostre a média.
-'''
-n1 = int(input("Digite a primeira nota\n"))
-n2 = int(input("Digite a segunda nota\n"))
-n3 = int(input("Digite a terceira nota\n"))
-n4 = int(input("Digite a quarta nota\n"))
-media = (n1+n2+n3+n4)/4
-print("A media é de:",media)
