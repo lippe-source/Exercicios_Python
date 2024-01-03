723dddf23fe767dbef98e0afa34008487b963086
lippe-source <81193430+lippe-source@users.noreply.github.com>
2024-01-03 17:56:51 -0300

    Delete exe08.py

--- exe08.py
+++
@@ -1,8 +0,0 @@
-'''
-Faça um Programa que pergunta quanto você ganha por hora e o número de
-horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês
-'''
-horas = int(input("Digite o numero de horas trabalhadas\n"))
-valor = float(input("Digite o valor das horas\n"))
-total = horas*valor
-print("O salário final é de:", total)
