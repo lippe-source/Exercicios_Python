4b101f20a07ddc80d597569b1be26384e08c3366
lippe-source <81193430+lippe-source@users.noreply.github.com>
2024-01-03 17:56:42 -0300

    Delete exe06.py

--- exe06.py
+++
@@ -1,6 +0,0 @@
-'''
-6. Faça um Programa que parte o raio de um círculo, calcule e mostre sua área.
-'''
-raio = float(input("Digite o valor do raio da circunferência\n"))
-area = 3.14 *(raio*raio)
-print("O valor da area é:", area)
